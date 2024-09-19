c1e72dcb36d41c309b49459def7a14d5ecc77a53
Neshan <kandelnishant01@gmail.com>
2024-09-19 13:39:44 +0545

    added py projects

---
+++ arith_calculator.py
@@ -0,0 +1,19 @@
+# Python Calculator
+operator = input("Enter an operator (+ - * /): ")
+num1 = float(input("enter the 1st number: "))
+num2 = float(input("enter the 2nd number: "))
+
+if operator == "+":
+    result= num1+num2
+    print (round(result, 3))
+elif operator == "-":
+    result = num1-num2
+    print (round(result, 3))
+elif operator == "*":
+    result = num1*num2
+    print (round(result, 3))
+elif operator == "/":
+    result = num1/num2
+    print (round(result, 3)) 
+else:
+    print(f"{operator} is not valid")
